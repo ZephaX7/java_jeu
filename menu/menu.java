c2a7accb1ed25abdf93d6a4cedadef1f2fc1c615
Theo <theo.delourneau@gmail.com>
2026-05-26 14:50:25 +0200

    jsp

--- menu/menu.java
+++ menu/menu.java
@@ -1,5 +1,42 @@
-public class menu {
+import java.util.Scanner;
+
+public class menu{
+
     public static void main(String[] args) {
-        System.out.println("test");
+
+        Scanner scanner = new Scanner(System.in);
+
+        boolean jeuEnCours = true;
+
+       
+        System.out.println("=== Bienvenue dans le jeu ===");
+
+        
+        while (jeuEnCours) {
+
+            System.out.println("1 - Jouer :");
+            System.out.println("2 - Quitter");
+
+            
+            String choix = scanner.nextLine();
+
+           
+            switch (choix) {
+
+                case "1":
+                    System.out.println("Bonne chance !");
+                    
+
+                case "2":
+                    jeuEnCours = false;
+                    break;
+
+                default:
+                    System.out.println("Commande inconnue.");
+            }
+        }
+
+       
+        scanner.close();
     }
 }
