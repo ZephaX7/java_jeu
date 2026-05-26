eda75f57b064f1a33f9aa1cfdaa75fae743d531a
ThomasBeyney <thomas.beyney@ynov.com>
2026-05-26 14:49:58 +0200

    menu 2

--- menu/menu.java
+++ menu/menu.java
@@ -1,5 +1,27 @@
+import java.util.Scanner;
+
 public class menu {
     public static void main(String[] args) {
-        System.out.println("test");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Bienvenue dans le menu du jeu !");
+
+        System.out.println(" ");
+        System.out.println("1. Jouer");
+        System.out.println("2. Quitter");
+        System.out.println(" ");
+
+        System.out.println("Veillez saisir quelque chose :");
+        System.out.println(" ");
+        String reponse = sc.nextLine();
+        
+        switch (reponse) {
+            case "1":
+                System.out.println("Bon jeu !");
+                break;
+
+            case "2":
+                System.out.println("Au revoir !");
+                break;
+        }
     }
 }
