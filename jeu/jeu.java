1d4f782db2b3605f7c7c330cfcdaf65105cf3b26
Theo <theo.delourneau@gmail.com>
2026-05-26 16:40:16 +0200

    indice

--- jeu/jeu.java
+++ jeu/jeu.java
@@ -12,29 +12,37 @@ public class jeu {
         Random random = new Random();
 
         String motAleatoire = liste.motus.get(random.nextInt(liste.motus.size()));
-        System.out.println("Mot choisi : " + "Mystere");
-        System.out.println("La première lettre est : " + motAleatoire.charAt(0));
+        System.out.println("Le mot mystere a ete choisi : ");
+        System.out.println("La premiere lettre est : " + motAleatoire.charAt(0));
+        
 
         boolean motTrouve = false;
         int tentatives = 6;
 
-        while (!motTrouve && tentatives > 0) {
+        while (!motTrouve && tentatives > 0) { 
             System.out.print("Votre proposition : ");
             String choix = sc.nextLine();
 
             if (choix.equals(motAleatoire)) {
-                System.out.println("Bravo, vous avez trouvé !");
+                System.out.println("Bravo, vous avez trouve !");
                 motTrouve = true;
                 break;
 
             } else {
-                tentatives--;
-                System.out.println("Mauvaise réponse. Tentatives restantes : " + tentatives);
-            }
+    tentatives--;
+    System.out.println("Mauvaise reponse. Tentatives restantes : " + tentatives);
+
+    if (tentatives == 3) {
+        System.out.println("Voici la liste des mots possibles :");
+        for (String mot : liste.motus) {
+            System.out.println("- " + mot);
+        }
+    }
+}
         }
 
         if (!motTrouve) {
-            System.out.println("Perdu ! Le mot était : " + motAleatoire);
+            System.out.println("Perdu ! Le mot etait : " + motAleatoire);
         }
         sc.close();
     }
